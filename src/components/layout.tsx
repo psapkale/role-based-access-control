5ecc23f6b93cd72ab8d2e4ec5473eca66bce5937
Prem <prempravinsapkale@gmail.com>
2024-11-22 20:27:35 +0530

    header responsive

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,37 +6,37 @@ import { useRecoilValue } from "recoil";
 import { loginState } from "@/store/atoms/login-state";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const isLoggedIn = useRecoilValue(loginState);
-  const { getRoleFromLocalStorage, clearRoleFromLocalStorage } = useUserRole();
+   const isLoggedIn = useRecoilValue(loginState);
+   const { getRoleFromLocalStorage, clearRoleFromLocalStorage } = useUserRole();
 
-  return (
-    <div className="p-6">
-      <div className="flex items-center justify-between mb-6">
-        <div>
-          <h1 className="text-3xl font-bold">User Management</h1>
-          <p className="text-muted-foreground">
-            Manage users and their access permissions
-          </p>
-        </div>
+   return (
+      <div className="p-6">
+         <div className="flex flex-col items-start gap-4 sm:flex-row sm:items-center sm:gap-0 justify-between mb-6">
+            <div>
+               <h1 className="text-3xl font-bold">User Management</h1>
+               <p className="text-muted-foreground">
+                  Manage users and their access permissions
+               </p>
+            </div>
 
-        {isLoggedIn && (
-          <div className="flex gap-4">
-            <span className="flex gap-2 items-center">
-              <span className="text-sm text-muted-foreground">
-                Current session as:
-              </span>
-              <Badge>{getRoleFromLocalStorage()}</Badge>
-            </span>
-            <Button variant={"ghost"} onClick={clearRoleFromLocalStorage}>
-              Logout
-            </Button>
-          </div>
-        )}
+            {isLoggedIn && (
+               <div className="flex gap-4">
+                  <span className="flex gap-2 items-center">
+                     <span className="text-sm text-muted-foreground">
+                        Current session as:
+                     </span>
+                     <Badge>{getRoleFromLocalStorage()}</Badge>
+                  </span>
+                  <Button variant={"ghost"} onClick={clearRoleFromLocalStorage}>
+                     Logout
+                  </Button>
+               </div>
+            )}
+         </div>
+
+         {children}
       </div>
-
-      {children}
-    </div>
-  );
+   );
 };
 
 export default Layout;
